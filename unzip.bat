607fc1a794dba69221e8298a167bcc721b05159e
GengGode <GengGode@outlook.com>
2022-12-08 13:21:18 +0800

    update script

--- unzip.bat
+++ unzip.bat
@@ -1,3 +1,4 @@
 cd pkg && call merge.bat && cd ..
 move .\pkg\qt.7z .\
 7z.exe x qt.7z
+7z.exe x .\3rdparty_pkg\openssl.7z -oC:/
---
+++ unzip.ps1
@@ -0,0 +1,5 @@
+# cd pkg && call merge.bat && cd ..
+# move .\pkg\qt.7z .\
+# 7z.exe x qt.7z
+.\7z.exe x pkg\5.15.7.7z.001 o.
+
