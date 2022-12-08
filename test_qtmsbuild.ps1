ad4aceb3ef6816a7ca5f1b21ce9732efed031718
GengGode <GengGode@outlook.com>
2022-12-08 13:31:41 +0800

    update pkg.

--- test_qtmsbuild.ps1
+++
@@ -1,8 +0,0 @@
-# 能否找到QtMsBuild/Qt.props
-
-$QtProps = "C:\Qt\5.12.0\msvc2017_64\QtMsBuild\Qt.props"
-if (Test-Path $QtProps) {
-    Write-Host "Found Qt.props"
-} else {
-    Write-Host "Not found Qt.props"
-}
--- unzip.ps1
+++
@@ -1,5 +0,0 @@
-# cd pkg && call merge.bat && cd ..
-# move .\pkg\qt.7z .\
-# 7z.exe x qt.7z
-.\7z.exe x pkg\5.15.7.7z.001 o.
-
